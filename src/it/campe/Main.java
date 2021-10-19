21a50f58c86a4c7436dec3a55158175559389e00
Ksenx73 <hello@itcampe.com>
2021-10-19 12:30:40 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,10 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки";
+        //напишите тут ваш код
+
+    }
+}
